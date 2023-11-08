27ce14b14d16415e7d7b1f8c13775dcf2c59c4e5
MetinPythonPro <147646944+MetinPythonPro@users.noreply.github.com>
2023-11-08 15:01:26 +0300

    BotA.py

--- BotA.py
+++ BotA.py
@@ -31,7 +31,7 @@ async def bye(ctx):
     await ctx.send(":slight_smile:")
 
 @bot.command()
-async def cool(ctx):
+async def coolbot(ctx):
     await ctx.send('Yes, the bot is cool.')
 
 @bot.command()
@@ -41,5 +41,34 @@ async def choose(ctx, *choices: str):
 @bot.command()
 async def coin(ctx):
     await ctx.send(yazi_tura())
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    await ctx.send(left + right)
+
+@bot.command()
+async def roll(ctx, dice: str):
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for q in range(rolls))
+    await ctx.send(result)
+
+@bot.command()
+async def repeat(ctx, times: int, content: str):
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}') # type: ignore
+
+@bot.group()
+async def cool(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
 
 bot.run(ayarlar["TOKEN"])
